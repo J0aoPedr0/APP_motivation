bc49e4bc4a2e87d174f32f1b825f83bc0683a043
João Pedro <joaobritosts@gmail.com>
2024-05-11 20:43:29 -0300

    Initial commit

---
+++ app/src/main/java/com/example/motivation/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.motivation
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.motivation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar
+
+        binding.buttonNewPhrase.setOnClickListener(this)
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onClick(view: View) {
+        Toast.makeText(this, "Estou funcionando", Toast.LENGTH_SHORT).show()
+    }
+}
